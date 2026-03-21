df60f39211b4fe91b979532259175eaddd33e9c5
Daniil <Dezmoynd@mail.ru>
2026-03-21 10:02:47 +1000

    [main]: added db envirements

--- promo_service/promo_service/settings.py
+++ promo_service/promo_service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +76,11 @@ WSGI_APPLICATION = "promo_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "promoservice",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.environ.get("DB_NAME", "postgres_db"),
+        "USER": os.environ.get("DB_USER", "postgres_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres_pwd"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
