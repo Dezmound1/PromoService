a7cca5e58750a91966ae098f8e223734a5716bd4
Daniil <Dezmoynd@mail.ru>
2026-03-23 01:49:06 +1000

    [main]: added serializers for orders

--- promo_service/orders/serializer.py
+++ promo_service/orders/serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from promo_service.goods.serializer import GoodSerializer
+
 from .models import Order
 
 from promo_service.promocode.serializers import DiscountPromoCodeSerializer
@@ -13,3 +15,15 @@ class CreateOrderSerializer(serializers.Serializer):
     class Meta:
         model = Order
         fields = ["user_id", "total_amount", "promo_code"]
+
+
+class OrderResponseSerializer(serializers.ModelSerializer):
+    """Serializer for the Order model."""
+
+    goods = GoodSerializer(many=True)  # TODO need to value fields of response
+    promo_code = DiscountPromoCodeSerializer()  # TODO need to value fields of response
+
+    class Meta:
+        model = Order
+        fields = ["id", "user", "goods", "promo_code", "total_price"]
+        read_only_fields = fields
--- promo_service/orders/views.py
+++ promo_service/orders/views.py
@@ -2,12 +2,14 @@ from rest_framework import status, viewsets
 from rest_framework.request import Request
 from django.http import JsonResponse
 
-from orders.repository import OrderRepository
+from promo_service.orders.serializer import CreateOrderSerializer
 
 
 class OrderViewSet(viewsets.ViewSet):
     def create(self, request: Request):
         try:
-            OrderRepository.create_order(request.POST)
+            order_serializer = CreateOrderSerializer(data=request.POST)
+            order_serializer.is_valid(raise_exception=True)
+            # TODO order = OrderService.create_order(order_serializer.validated_data)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
